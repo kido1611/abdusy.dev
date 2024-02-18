642b15772798d0e17f805f5e7c1f292c4510a78a
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-02-18 14:25:15 +0700

    feat: add connection file

---
+++ utils/db.ts
@@ -0,0 +1,15 @@
+import { H3Event } from "h3";
+import { drizzle } from "drizzle-orm/libsql";
+
+import { createClient } from "@libsql/client";
+
+export default (event?: H3Event) => {
+  const runtimeConfig = useRuntimeConfig(event);
+
+  const connection = createClient({
+    url: runtimeConfig.tursoUrl,
+    authToken: runtimeConfig.tursoAuthToken,
+  });
+
+  return drizzle(connection);
+};
