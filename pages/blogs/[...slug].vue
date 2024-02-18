9cda909ab32a5aaffa38737b66c46139a23e1833
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-02-18 19:34:51 +0700

    feat: replace blog component with custom queryContent

--- pages/blogs/[...slug].vue
+++ pages/blogs/[...slug].vue
@@ -1,19 +1,40 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute();
+
+const { data, error, pending, refresh } = await useAsyncData(
+  `blogs-${route.path}`,
+  () => queryContent().where({ _path: route.path }).findOne()
+);
+if (data.value) {
+  useContentHead(data.value);
+}
+</script>
 
 <template>
-  <main>
-    <ContentDoc>
-      <template #default="{ doc }">
-        <section id="toc">
-          <ul>
-            <li v-for="toc in doc.body?.toc?.links" :key="toc.id">
-              <NuxtLink :to="`#${toc.id}`">{{ toc.text }}</NuxtLink>
-            </li>
-          </ul>
-        </section>
-        <hr />
-        <ContentRenderer :value="doc" class="prose mx-auto" />
-      </template>
-    </ContentDoc>
+  <main class="p-4 md:p-6 grid grid-cols-1 gap-6 content-blog">
+    <section v-if="data" id="toc" class="bg-gray-800 p-4 rounded-md">
+      <label
+        for="toc-list"
+        class="text-xs text-gray-200 font-medium tracking-wide"
+        >Daftar Isi</label
+      >
+      <nav id="toc-list" name="Daftar Isi" class="mt-2">
+        <ul class="space-y-1 *:text-sm *:text-white hover:*:underline">
+          <li v-for="toc in data?.body?.toc?.links" :key="toc.id">
+            <NuxtLink :to="`#${toc.id}`">{{ toc.text }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+    <ContentRenderer :value="(data as any)" tag="article" class="prose mx-auto">
+      <template #empty> error </template>
+    </ContentRenderer>
+
+    <pre>
+      error: {{ error }}
+    </pre>
+    <pre>
+      pending: {{ pending }}
+    </pre>
   </main>
 </template>
--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -2,30 +2,55 @@
   <div>
     <h1>Blogs</h1>
     <div class="flex flex-col space-y-4 max-w-[70ch] mx-auto">
-      <ContentList :query="query" v-slot="{ list }">
-        <NuxtLink
-          :to="blog._path"
-          v-for="blog in list"
-          :key="blog._path"
-          class="border border-gray-300 rounded p-4"
-        >
-          <h2>{{ blog.title }}</h2>
-          <p>{{ blog.description }}</p>
-        </NuxtLink>
-      </ContentList>
+      <NuxtLink
+        v-for="blog in blogs"
+        :key="blog._path"
+        :to="blog._path"
+        class="block border border-gray-300 rounded p-4"
+      >
+        <h2>{{ blog.title }}</h2>
+        <p>{{ blog.description }}</p>
+        <p>{{ blog.author }}</p>
+        <p>
+          Time:
+          <time :datetime="blog.created_at">{{ blog.created_at }}</time>
+        </p>
+      </NuxtLink>
     </div>
+    <pre>
+      error: {{ error }}
+    </pre>
+    <pre>
+      pending: {{ pending }}
+    </pre>
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { QueryBuilderParams } from "@nuxt/content/types";
-const query: QueryBuilderParams = {
-  sort: [
-    {
-      created_at: -1,
-    },
-  ],
-};
+useSeoMeta({
+  title: "Blogs",
+  description: "Anything I have written",
+  ogTitle: "Blogs",
+  ogDescription: "Anything I have written",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "id",
+  },
+});
+
+const {
+  data: blogs,
+  error,
+  pending,
+  refresh,
+} = useAsyncData("blogs", () =>
+  queryContent("blogs")
+    .sort({ created_at: -1 })
+    .only(["_path", "title", "description", "author", "created_at"])
+    .find()
+);
 </script>
 
 <style></style>
