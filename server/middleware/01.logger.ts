12526c60bec6d82e24411e0963365c17d0dd03bb
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-02-18 14:25:27 +0700

    feat: add logger middleware

---
+++ server/middleware/01.logger.ts
@@ -0,0 +1,13 @@
+import db from "~/utils/db";
+import { logs } from "~/db/schema";
+
+export default defineEventHandler(async (event) => {
+  const ip = getRequestIP(event, { xForwardedFor: true });
+
+  await db(event)
+    .insert(logs)
+    .values({
+      ipAddress: ip || "",
+      path: event.path,
+    });
+});
