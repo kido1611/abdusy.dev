bf82c835937352f5538227107d0b55a16f21d3b1
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-03-09 18:44:57 +0700

    feat: remove unused code in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-      >Learn more about Nuxt Routing</a
-    >
-
-    <NuxtWelcome />
-  </div>
+  <NuxtWelcome />
 </template>
