5fe7523dae5ecfbd2f526268105ee38edc196690
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-05-31 18:45:29 +0700

    fix: turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config'
-import type { Config } from 'drizzle-kit'
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: './db/schema.ts',
-  out: './drizzle',
-  driver: 'turso',
+export default defineConfig({
+  schema: "./server/db/schema.ts",
+  out: "./server/db/migrations",
+  dialect: "sqlite",
   dbCredentials: {
-    url: process.env.NUXT_TURSO_URL ?? '',
-    authToken: process.env.NUXT_TURSO_AUTH_TOKEN ?? '',
+    url: process.env.NUXT_TURSO_URL ?? "",
+    authToken: process.env.NUXT_TURSO_AUTH_TOKEN ?? "",
   },
-} satisfies Config
+});
--- server/api/logs/index.get.ts
+++ server/api/logs/index.get.ts
@@ -1,12 +1,17 @@
-import { desc, notInArray } from 'drizzle-orm'
+import { desc, notInArray } from "drizzle-orm";
 
-import db from '~/utils/db'
-import { logs } from '~~/db/schema'
+import db from "~/utils/db";
+import { logs } from "~~/server/db/schema";
 
 export default defineEventHandler(async (event) => {
-  const selectLogs = await db(event).select().from(logs).where(notInArray(logs.ipAddress, ['103.127.136.3'])).orderBy(desc(logs.createdAt)).limit(50)
+  const selectLogs = await db(event)
+    .select()
+    .from(logs)
+    .where(notInArray(logs.ipAddress, ["103.127.136.3"]))
+    .orderBy(desc(logs.createdAt))
+    .limit(50);
 
   return {
     logs: selectLogs,
-  }
-})
+  };
+});
