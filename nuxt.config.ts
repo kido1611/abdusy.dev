6a77e0e102606b0d0d6320f5941f3fd70bbc7b92
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-05-31 17:27:35 +0700

    chore: remove prettier and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: {
-    enabled: true
+    enabled: true,
   },
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    '@vueuse/nuxt',
-    '@nuxt/content',
-    '@pinia-plugin-persistedstate/nuxt',
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    "@nuxt/image"
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/content",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxt/icon",
+    "@nuxt/image",
   ],
   content: {
     highlight: {
-      theme: 'github-dark'
-    }
+      theme: "github-dark",
+    },
   },
   runtimeConfig: {
-    tursoUrl: '',
-    tursoAuthToken: '',
-    isLoggingEnabled: false
+    tursoUrl: "",
+    tursoAuthToken: "",
+    isLoggingEnabled: false,
   },
   routeRules: {
-    '/': { prerender: true },
-    '/blogs/**': { prerender: true, static: true },
-    '/api/**': { cors: true }
-  },
-  eslint: {
-    config: {
-      stylistic: true
-    }
+    "/": { prerender: true },
+    "/blogs/**": { prerender: true, static: true },
+    "/api/**": { cors: true },
   },
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
-  compatibilityDate: '2024-07-29'
-})
+  compatibilityDate: "2024-07-29",
+});
+
