bcb0bdeab5bdf98f9c1222cff23d90b4a308dfdb
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-06-01 15:54:43 +0700

    chore: change default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,58 +1,13 @@
+<script lang="ts" setup>
+useHead({
+  bodyAttrs: {
+    class: "bg-neutral-900 text-white",
+  },
+});
+</script>
+
 <template>
-  <div>
-    <header>
-      <div class="container px-4 py-3 mx-auto">
-        <div class="flex flex-row justify-between items-center">
-          <p>Header</p>
-
-          <nav>
-            <ol class="inline-flex flex-row items-center">
-              <li>
-                <NuxtLink
-                  class="px-4 py-2.5 rounded-md hover:bg-sky-200 block"
-                  to="/"
-                >
-                  Home
-                </NuxtLink>
-              </li>
-              <li>
-                <NuxtLink
-                  class="px-4 py-2.5 rounded-md hover:bg-sky-200 block"
-                  to="/blogs"
-                >
-                  Blogs
-                </NuxtLink>
-              </li>
-              <li>
-                <NuxtLink
-                  class="px-4 py-2.5 rounded-md hover:bg-sky-200 block"
-                  to="/examples"
-                >
-                  Examples
-                </NuxtLink>
-              </li>
-            </ol>
-          </nav>
-        </div>
-
-        <CurrentRoute />
-      </div>
-    </header>
-    <hr>
-    <slot />
-
-    <footer>
-      <p>
-        This site was created using
-        <Icon
-          name="vscode-icons:file-type-nuxt"
-          size="28"
-        />
-      </p>
-    </footer>
-  </div>
+  <slot />
 </template>
-
-<script lang="ts" setup></script>
 
 <style></style>
